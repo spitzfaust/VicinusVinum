5ee9d2d71f0d6c6f11e46c3992a2532323159183
Tobias Watzek <spitzfaust@gmail.com>
2017-03-10 18:15:38 +0100

    Added empty test classes

---
+++ src/test/java/vicinusvinum/knn/EuklidianDistanceCalculatorTest.java
@@ -0,0 +1,16 @@
+package vicinusvinum.knn;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tobias.
+ */
+public class EuklidianDistanceCalculatorTest {
+    @Test
+    public void calculate() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/vicinusvinum/knn/ManhattanDistanceCalculatorTest.java
@@ -0,0 +1,16 @@
+package vicinusvinum.knn;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tobias.
+ */
+public class ManhattanDistanceCalculatorTest {
+    @Test
+    public void calculate() throws Exception {
+
+    }
+
+}
